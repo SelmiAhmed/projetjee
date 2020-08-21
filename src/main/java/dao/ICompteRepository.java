a7612fba506a1a24bd04d1ec6e243f2a8fe42fc7
AhmedSelmi <ahmed_selmi@outlook.com>
2020-08-21 12:37:52 +0200

    jee

---
+++ src/main/java/dao/ICompteRepository.java
@@ -0,0 +1,11 @@
+package dao;
+
+import entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+
+public interface ICompteRepository extends JpaRepository<Compte,String>  {   //interface qui herite d'une interface  -----  le ID de compte est de type String (2 eme parametre)
+
+}
